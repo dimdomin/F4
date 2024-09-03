922b776d10556c4487622b67181df0bea7b040d8
dimdomin <145801131+dimdomin@users.noreply.github.com>
2024-09-03 21:54:56 +0300

    Delete Frontend directory

--- Frontend/src/const/const.js
+++
@@ -1,4 +0,0 @@
-export const URL_Categories = "http://127.0.0.1:8000/api/categories/";
-export const URL_Dishes = "http://127.0.0.1:8000/api/dishes/?category=";
-
-export const URL_Recipe = "http://127.0.0.1:8000/api/recipes/";
